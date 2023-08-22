8af76daee68c1d1a069d6999dbc206012469a367
Vision <74461032+Vision84@users.noreply.github.com>
2023-08-22 15:33:41 -0400

    fixed issues with api and created a way for django to interface with firebase

--- django_application/api/views.py
+++ django_application/api/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import *
+
+import json
 
 def index(request):
     return HttpResponse("<h1>Index</h1>")
 
-def apiGetLessons(request):
-    lessons = Lesson.objects.all().order_by('-timestamp').values()
+@csrf_exempt
+def createUser(request):
+    if request.method == "POST":
+        body = json.loads(request.body.decode('utf-8'))
+        uid = body.get("uid")
 
-    return serializers.serialize("json", lessons)
+        try:
+            u = User(uid=uid)
+            u.save()
+
+            return JsonResponse({"success": "OK"})
+        
+        except Exception as e:
+            return JsonResponse({"error": "Unknown Error Occurred"})
+    
+    else:
+        return JsonResponse({"error": f"This API does not accept {request.method} requests."})
+
+@csrf_exempt
+def apiGetLessons(request):
+    if request.method == "GET":
+        lessons = Lesson.objects.all().order_by('-timestamp').values()
+        serialized = json.loads(serializers.serialize("json", lessons))
+
+        return JsonResponse({"lessons": serialized})
+    else:
+        return JsonResponse({"error": f"This API does not accept {request.method} requests."})
